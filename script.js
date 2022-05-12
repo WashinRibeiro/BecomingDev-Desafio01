5ba070ef60ca497536154edb4c2b9888ca639a16
Washington Ribeiro da Silva <washinuchiha@gmail.com>
2022-05-12 03:10:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function eu() {
+    text = document.getElementById("button_green");
+
+    if (text.style.visibility == "visible") {
+        text.style.visibility = "hidden";
+    } else {
+        text.style.visibility = "visible";
+    }
+}
+
+function vou() {
+    text = document.getElementById("button_yellow");
+
+    if (text.style.visibility == "visible") {
+        text.style.visibility = "hidden";
+    } else {
+        text.style.visibility = "visible";
+    }
+}
+
+function virar() {
+    text = document.getElementById("button_blue");
+
+    if (text.style.visibility == "visible") {
+        text.style.visibility = "hidden";
+    } else {
+        text.style.visibility = "visible";
+    }
+}
+
+function dev() {
+    text = document.getElementById("button_red");
+
+    if (text.style.visibility == "visible") {
+        text.style.visibility = "hidden";
+    } else {
+        text.style.visibility = "visible";
+    }
+} 
